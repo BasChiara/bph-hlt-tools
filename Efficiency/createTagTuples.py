374d3a5877deef0a7741311485ad0e4e1038adce
Chiara Basile <basilechiara@libero.it>
2025-03-04 16:55:26 +0100

    [+] run efficiency with config files inputs

--- Efficiency/createTagTuples.py
+++ Efficiency/createTagTuples.py
@@ -68,11 +68,19 @@ my_parser.add_argument('--tagPath',
 my_parser.add_argument('--data_path',
                     type = str,
                     default='/eos/user/h/hcrottel/BPHTriggerTuples/ParkingDoubleMuonLowMass3/BPHTriggerTuples_ParkingDoubleMuonLowMass3-Run2024C-PromptReco-v1HLTStudy/*/*/*root',
-                    help='path of the input files')
+                    help='folder path containing the input files')
+my_parser.add_argument('--output_path',
+                    type = str,
+                    default='/eos/cms/store/group/phys_bphys/cbasile/BPH_TagTuples/',
+                    help='path of the output folder')
 my_parser.add_argument('--name',
                     type = str,
                     default='',
                     help='path of the input files')
+my_parser.add_argument('--year',
+                    type = str,
+                    default='2024',
+                    help='year of the data')
 my_parser.add_argument('--dryrun',
                     action='store_true',
                     help='Print the commands without running them')
@@ -83,33 +91,35 @@ print(args)
 
 tagPath=args.tagPath
 data_path=args.data_path + '*root'
+name = args.name
 for indx, k in enumerate(data_path.split('/')):
     print(k)
     if 'BPHTriggerTuples_' in k: break
-
-name = args.name
-if args.dryrun:
-    dataset='JPsiP8'
-    sample=""
-    year="2023"
-    version="mc" 
-else:
-    dataset=data_path.split('/')[indx-1].replace('*','')
-    sample=data_path.split('/')[-2].replace('*','')
-    year=data_path.split('/')[indx].split('-')[1]
-    version=data_path.split('/')[indx].split('-')[-1]
-data_name=f'{name+"_" if name else ""}{dataset}_{year}_{version}_{tagPath}{"_"+sample if sample else ""}'
+dataset_info = data_path.split('/')
+dataset_wrapper = {
+    'dataset': dataset_info[indx-1].replace('*',''),
+    'sample': dataset_info[indx].replace('*',''),
+    'year': args.year if args.year else dataset_info[indx].split('-')[1],
+    'version': dataset_info[-2].split('-')[-1],
+}
+print(dataset_wrapper)
+data_name=f'{name+"_" if name else ""}{dataset_wrapper["dataset"]}_{dataset_wrapper["year"]}_{dataset_wrapper["sample"]}_{tagPath}{"_"+dataset_wrapper["version"] if dataset_wrapper["version"] else ""}'
 print(data_name)
 
 bad_files = []
 
 files = glob.glob(data_path)
 print('Total files: ', len(files))
-
-main_path = f'/eos/user/c/cbasile/BPH_trigger/TagTuples/Run3_MC/' if not args.dryrun else os.getcwd()
+main_path = args.output_path
 os.makedirs(main_path, exist_ok=True)
-output_file = f'{main_path}/{data_name}.root'
+output_file = f'{main_path}{data_name}.root'
 time.sleep(5)
+
+if args.dryrun:
+    print('---- DRY RUN ----')
+    print('Output file:', output_file)
+    print('-----------------')
+    exit()
 
 dataTag = list()
 for indx, fn in enumerate(files):
@@ -130,6 +140,7 @@ for indx, fn in enumerate(files):
     print('[+] data Loaded')
     muonFiringTag = data.filter(regex=f'mu.*{tagPath}').sum(axis=1)>0 # mask : at least one muon fired the tagHLT
     dataTag_ = data.loc[muonFiringTag]
+    #dataTag_.set_index('event', inplace=True)
 
     # rename 'mu1' -> muTag, 'mu2' -> muProbe 
     dataTag_leadingTag  = (dataTag_.loc[dataTag_[f'mu1_{tagPath}']==1]).copy()
---
+++ Efficiency/utils.py
@@ -0,0 +1,114 @@
+# utility function for  efficiency calculation
+import json
+import numpy as np
+
+from scipy import stats
+
+########################
+# ----  FUNCTIONS ---- #
+########################
+
+def clopper_pearson(x, n, alpha=0.32, return_errors=True):
+    """Estimate the confidence interval for binomial distributions.
+    `x` is the number of successes and `n` is the number trials (x <=
+    n). `alpha` is the confidence level (i.e., the true probability is
+    inside the confidence interval with probability 1-alpha). The
+    function returns a `(low, high)` pair of numbers indicating the
+    interval on the probability.
+    https://root.cern.ch/doc/master/classTEfficiency.html#ae80c3189bac22b7ad15f57a1476ef75b
+    """
+    b = stats.beta.ppf
+    #if isinstance(x, np.ndarray):
+    #    alpha = alpha*np.ones_like(x)
+    ratio = x / n
+    ratio = np.nan_to_num(ratio, nan=0)
+    
+    lo = b(alpha / 2, x, n - x + 1)
+    hi = b(1 - alpha / 2, x + 1, n - x)
+    if isinstance(x, np.ndarray):
+        lo = np.nan_to_num(lo, nan=0)
+        hi = np.nan_to_num(hi, nan=1)
+        if return_errors:
+            lo = ratio - lo
+            hi = hi -ratio
+            hi = np.where((ratio==0) & (hi>0.5), 0, hi )
+        to_return = np.array([lo, hi])
+    else:
+        to_return = [0.0 if np.isnan(lo) else lo, 
+                    1.0 if np.isnan(hi) else hi]
+        if return_errors:
+            to_return[0] = ratio - to_return[0]
+            to_return[1] = to_return[1] -ratio
+    return to_return
+
+class npEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        if isinstance(obj, np.floating):
+            return float(obj)
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return super(npEncoder, self).default(obj)
+
+def passfail_histo(data, var, denQuery, numQuery, probePath, Bins1d, norm=False):
+
+    h_all      = np.histogram(data.query(denQuery)[var], bins=Bins1d[var], density=norm)
+    ccomplete_numQuery = denQuery
+    if numQuery:
+        ccomplete_numQuery += " & "+numQuery 
+    ccomplete_numQuery+= f' & muProbe_{probePath}==1'
+    h_passprob = np.histogram(data.query(ccomplete_numQuery)[var], bins=Bins1d[var], density=norm) 
+
+    return h_all, h_passprob
+
+def get_and_store(data, var, denQuery, numQuery, tagPath, probePath, Bins1d, input_file, outputdir, v_name=''):
+        
+    h_all, h_passprob = passfail_histo(data, var, denQuery, numQuery, probePath, Bins1d)     
+    ratio = h_passprob[0]/h_all[0]
+    err  = clopper_pearson(h_passprob[0], h_all[0])
+
+    efficiency_output = dict(
+        Bins    = h_all[1],
+        Passing = h_passprob[0],
+        All     = h_all[0],
+        Ratio   = ratio,
+        Error   = err,
+        Probe   = probePath,
+        Tag     = tagPath,
+        DenQuery= denQuery,
+        NumQuery= numQuery,
+        Var     = var,
+        input   = input_file
+    )
+    v_name = f'_v{v_name}' if v_name else ''
+    with open(f'{outputdir}/Efficiency1D_Tag{tagPath}Probe{probePath}_{var}{v_name}.json', 'w+') as jj:
+        json.dump(efficiency_output, jj, indent=4, cls=npEncoder)
+    
+    return h_all[1], ratio, err
+
+def get_efficiency2D(data, var, denQuery, numQuery, tagPath, probePath, Bins_xy, norm=False):
+    
+    xedges = np.array(Bins_xy[0])
+    yedges = np.array(Bins_xy[1])
+    passprobeQuery = ' & '.join([denQuery, numQuery])
+    
+    h_passprob  = np.histogram2d(
+        data.query(passprobeQuery)[var[0]], 
+        data.query(passprobeQuery)[var[1]], 
+        bins=[xedges, yedges],
+        weights=data.query(passprobeQuery)[f'muProbe_{probePath}'],
+        density=norm)
+    h_all = np.histogram2d(
+        data.query(denQuery)[var[0]], 
+        data.query(denQuery)[var[1]], 
+        bins=[xedges, yedges],
+        density=norm)
+    
+    ratio  = np.divide(
+        h_passprob[0], 
+        h_all[0], 
+        out=np.zeros_like(h_passprob[0]), 
+        where=h_all[0] != 0)
+
+    return h_all, h_passprob, ratio
